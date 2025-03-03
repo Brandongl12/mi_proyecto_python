20738cd28c49486fc0ac078a7e6257bb327116f5
Brandongl12 <branlevario@gmail.com>
2025-03-02 18:08:19 -0600

    Agregada funciòn despedir en main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+
+
 def saludar(brandon):
+
+def despedir(brandon):
+    print(f"Adiòs, {brandon}!")
+
+def preguntar_como_estas(brandon):
+    print(f¿Como estas, {brandon}?")
+
+
 if __name__ == "__main__":
+   sakudae("Mundo")
+   preguntar_como_estas("Mundo")
+   despedir("Mundo)
